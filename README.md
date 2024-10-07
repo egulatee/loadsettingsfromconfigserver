0a16af304629007a64e86a5a920756494220b9e5
eric.gulatee <eric.gulatee@gmail.com>
2024-10-07 02:05:07 -0400

    Initial commit. Supports loading and setting env from a config server (Spring Config Server_ Also uses Access Tokens to authenticate

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# loadfromconfigserver
+# actionenvfromspringconfigserver
+
+This allows one to set environment variables in your workflow from a spring configuration server.
+
+Authentication to the config server is done using oauth's access token.
+
+Currently it can only set environment variables.  It does not currently support setting secrets.
+
+## TODO Figure out how to set a secret
+## https://github.com/hmanzur/actions-set-secret
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import * as core from "@actions/core";
+
+const baseurl = core.getInput("cloud_config_server_base_url");
+const path = core.getInput("path");
+const property = core.getInput("propertytoretrieve");
+const variabletoset = core.getInput("variabletoset");
+//const environmentvariabletoset = core.getInput("environmentvariabletoset");
+//const secretvariabletoset = core.getInput("secretvariabletoset");
+const outputassecret = core.getInput("outputassecret");
+const outputasenvvar = core.getInput("outputasenvvar");
+
+const authtoken = core.getInput("authtoken");
+
+let url = baseurl + path;
+console.log(`Connecting to ${url}`);
+
+fetch(url, {
+//  method: "POST",
+  headers: {
+    Authorization: "Bearer " + getAuthToken(),
+  },
+})
+  .then((response) => {
+    processResponse(response);
+  })
+  .catch((error: Error) => {
+    core.error("Failed to connect to " + url);
+    core.setFailed(`Failed to connect to ${url}`);
+  });
+
+async function processResponse(response: Response) {
+  if (response.status === 200) {
+    core.debug("Successfully fetched cloud config!");
+    let json = await response.json();
+    console.log("json=" + JSON.stringify(json));
+
+    let propertySource = json["propertySources"];
+    console.log("propertySource=" + JSON.stringify(propertySource));
+    let source = propertySource[0]["source"];
+    console.log("source=" + JSON.stringify(source));
+    let value = source[property];
+    console.log("value=" + value);
+
+    if (outputasenvvar === "true") {
+      console.log("outputasenvvar");
+      console.log("setting[" + variabletoset + "] to value[" + value + "]");
+      core.exportVariable(variabletoset, value);
+      //        core.setOutput(environmentvariabletoset, value);
+    }
+    if (outputassecret === "true") {
+      console.error("Not implemented");
+      core.setFailed("Setting secret isn't implemented");
+      // console.log("outputassecret")
+      // console.log("setting[" + environmentvariabletoset +"] to value[" + value + "]");
+      // core.setSecret(value);
+      // core.exportVariable(environmentvariabletoset, value)
+    }
+  } else {
+    core.error("Failed to fetch cloud config!");
+    throw new Error("Failed to fetch cloud config!");
+  }
+}
+function getAuthToken() {
+  return authtoken;
+}
