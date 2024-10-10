919cfd0f1f0b45b33b4c63e0410618602f2e38d5
eric.gulatee <eric.gulatee@gmail.com>
2024-10-09 23:22:07 -0400

    GitHub Token

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,48 @@
 import * as core from "@actions/core";
-import * as github from '@actions/github';
+import * as github from "@actions/github";
 
-const AUTH_TOKEN_ENDPOINT = core.getInput("AUTH_TOKEN_ENDPOINT")
-const CLIENT_ID = core.getInput("CLIENT_ID")
-const CLIENT_SECRET = core.getInput("CLIENT_SECRET")
+const AUTH_TOKEN_ENDPOINT = core.getInput("AUTH_TOKEN_ENDPOINT");
+const CLIENT_ID = core.getInput("CLIENT_ID");
+const CLIENT_SECRET = core.getInput("CLIENT_SECRET");
 
-const token = core.getInput('token', { required: true });
-const baseurl = core.getInput("cloud_config_server_base_url");
-const path = core.getInput("path");
-const property = core.getInput("propertytoretrieve");
-const variabletoset = core.getInput("variabletoset");
+const token = core.getInput("GITHUB_TOKEN", { required: true });
 
-//const maskassecret = stringToBoolean(core.getInput("maskassecret"));
-const outputasenvvar = stringToBoolean(core.getInput("outputasenvvar"));
-const outputassecret = stringToBoolean(core.getInput("outputasecret"));
-const decodebase64 = stringToBoolean(core.getInput("decodebase64"));
+const baseurl = core.getInput("cloud_config_server_base_url", {
+  required: true,
+});
+const path = core.getInput("path", { required: true });
+const property = core.getInput("propertytoretrieve", { required: true });
+const variabletoset = core.getInput("variabletoset", { required: false });
 
-import { createOrUpdateSecretForRepo } from './github-api';
+const outputasenvvar = stringToBoolean(
+  core.getInput("outputasenvvar", { required: false })
+);
+const outputassecret = stringToBoolean(
+  core.getInput("outputasecret", { required: false })
+);
+const decodebase64 = stringToBoolean(
+  core.getInput("decodebase64", { required: false })
+);
 
-main()
+import { createOrUpdateSecretForRepo } from "./github-api";
+
+main();
 
 async function main() {
-  let accessToken = await getToken(AUTH_TOKEN_ENDPOINT, CLIENT_ID, CLIENT_SECRET);
-//  console.log("Access Token=" + accessToken)
+  let accessToken = await getToken(
+    AUTH_TOKEN_ENDPOINT,
+    CLIENT_ID,
+    CLIENT_SECRET
+  );
+  //  console.log("Access Token=" + accessToken)
   connectToConfigServer(baseurl, path, accessToken);
 }
 
-async function connectToConfigServer(baseurl: string, path: string, accessToken: string) {
+async function connectToConfigServer(
+  baseurl: string,
+  path: string,
+  accessToken: string
+) {
   let url = baseurl + path;
   console.log(`Connecting to ${url}`);
   fetch(url, {
@@ -69,26 +85,28 @@ async function processResponse(response: Response) {
       core.setSecret(value);
     }
 
-    let varname
+    let varname;
     if (isUndefinedEmptyOrNull(variabletoset)) {
-      varname = property
-    }
-    else {
-      varname = variabletoset
+      varname = property;
+    } else {
+      varname = variabletoset;
     }
 
     if (outputasenvvar === true) {
       core.exportVariable(varname, value);
-      core.setOutput('result', 'Environment Variable [' + varname + "] set to value["  + value  + "]");
+      core.setOutput(
+        "result",
+        "Environment Variable [" + varname + "] set to value[" + value + "]"
+      );
     }
     if (outputassecret === true) {
       // console.error("Not implemented");
       // core.setFailed("Setting secret isn't implemented");
       const octokit = github.getOctokit(token);
       const { owner, repo } = github.context.repo;
-  
+
       await createOrUpdateSecretForRepo(octokit, owner, repo, varname, value);
-      core.setOutput('result', 'Secret [' + varname + '] set successfully');
+      core.setOutput("result", "Secret [" + varname + "] set successfully");
     }
   } else {
     core.error("Failed to fetch cloud config!");
@@ -96,15 +114,13 @@ async function processResponse(response: Response) {
   }
 }
 
-
 function stringToBoolean(str: string): boolean {
-  return str
-      .toLowerCase() === 'true';
+  return str.toLowerCase() === "true";
 }
 
 function isUndefinedEmptyOrNull(str: string): boolean {
-  if (str === undefined || str === null || str === '') {
+  if (str === undefined || str === null || str === "") {
     return true;
   }
-  return false
+  return false;
 }
